a7e8615c2e4323e6c18e7a557059d94b66a8e6f1
John Smith <private@unknown.com>
2012-12-28 18:14:35 -0800

    Created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exoticfortress::Application.config.secret_token = 'c41286d342310a57e7369455d028760ad6b7a8e92b9191db46ba3cc00df5c7bfb7839d4ef6d26c41c5bba8c5d7f7b4ab6626f41ef040f8447a7cec5a0d698280'
